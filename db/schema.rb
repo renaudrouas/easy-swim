9c44d1284f8c1d00021eea08cb5aee4d02cfc99b
renaud rouas <renaud.rouas@gmail.com>
2018-05-16 00:20:21 +0200

    update des views pools + navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_100814) do
+ActiveRecord::Schema.define(version: 2018_05_15_211033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2018_05_15_100814) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "title"
+    t.string "address"
     t.index ["user_id"], name: "index_pools_on_user_id"
   end
 
