40e57a1e45733bae2c56431069d3f2870c6a3e8f
Anaisl14 <laurentanais2@gmail.com>
2018-05-15 12:09:00 +0200

    create model booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_093351) do
+ActiveRecord::Schema.define(version: 2018_05_15_100814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "pool_id"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pool_id"], name: "index_bookings_on_pool_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "pools", force: :cascade do |t|
     t.string "dimensions"
@@ -48,5 +58,7 @@ ActiveRecord::Schema.define(version: 2018_05_15_093351) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "pools"
+  add_foreign_key "bookings", "users"
   add_foreign_key "pools", "users"
 end
