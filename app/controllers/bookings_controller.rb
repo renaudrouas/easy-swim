e2893dddca8db017a3ac0561030646a2b505d8f6
renaud rouas <renaud.rouas@gmail.com>
2018-05-15 15:46:24 +0200

    controllers créés

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,48 @@
+class BookingsController < ApplicationController
+
+
+ def index
+  @pool = Pool.find(params[:pool_id])
+  @booking = Booking.all
+end
+
+
+
+
+def new
+  @pool = Pool.find(params[:pool_id])
+  @booking = Booking.new
+end
+
+def create
+  @pool = Pool.find(params[:pool_id])
+  @booking = Booking.new(booking_params)
+  @booking.pool = @pool
+  if @booking.save
+    redirect_to pool_path(@pool)
+  else
+    render :new
+  end
+end
+
+def destroy
+  @pool = Pool.find(params[:id])
+  @booking.destroy
+  redirect_to pool_path(@booking.pool)
+end
+
+private
+
+def booking_params
+  params.require(:booking).permit(:date)
+end
+
+
+
+
+
+
+
+
+
+end
