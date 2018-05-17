601371690560a763627bf179a47a1479f90d167d
renaud rouas <renaud.rouas@gmail.com>
2018-05-17 17:06:20 +0200

    modif autorisations

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -5,8 +5,8 @@ class Pool < ApplicationRecord
 
 mount_uploader :photo, PhotoUploader
 
-validates :address, presence: true, uniqueness: true
-validates :description, presence: true
+validates :address, presence: true #uniqueness: true
+validates :description #presence: true
 validates :price, presence: true
 validates :title, presence: true
 validates :dimensions, presence:true
