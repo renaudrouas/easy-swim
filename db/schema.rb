1256d71d5e51d79da5d04833ab43927c6e703260
renaud rouas <renaud.rouas@gmail.com>
2018-05-17 22:50:18 +0200

    API Figaro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_17_095817) do
+ActiveRecord::Schema.define(version: 2018_05_17_204050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2018_05_17_095817) do
     t.string "phone_number"
     t.string "avatar"
     t.string "photo"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
